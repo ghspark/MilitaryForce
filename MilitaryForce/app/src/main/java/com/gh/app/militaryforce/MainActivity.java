a1332913925cb8112bc665b93c407ea2f5b95c07
gaohang <gaohang18@foxmail.com>
2015-09-21 00:14:39 +0800

    finish the head of the mainActivity

--- MilitaryForce/app/src/main/java/com/gh/app/militaryforce/MainActivity.java
+++ MilitaryForce/app/src/main/java/com/gh/app/militaryforce/MainActivity.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 
 import com.gh.app.militaryforce.base.BaseActivity;
 
+import butterknife.ButterKnife;
+
 public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
 }
